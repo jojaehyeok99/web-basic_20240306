1ab007a989cacc9bdc732c91a546b9df30c25a61
jojaehyeok99 <choco5366@naver.com>
2024-04-02 14:06:33 +0900

    2024 - 04 - 02 typescript : 인터페이스, type_alias, 제너릭

--- ts/5.type_alias/type_alias.ts
+++ ts/5.type_alias/type_alias.ts
@@ -1,9 +1,20 @@
 /*
-    타입 앨리어스 :
-    - 
-    - 
+  타입 앨리어스 :
+  - 커스텀 타입을 지정하는 방법으로 인터페이스와 유사하게 사용됨
+  - type 키워드를 사용
 */
+// let gender: '남' | '여' ;
 
+type Gender = '남' | '여';
+let gender: Gender = '남';
 
+type CustomText = string;
+
+type King = {
+  name : string,
+  tombName : string;
+  birth : string;
+  death : string
+};
 
 console.log('==================================================');
